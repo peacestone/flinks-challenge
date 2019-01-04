583b7d25886dd2e7594198b9d27326567d0843c9
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2019-01-03 21:41:26 -0500

    request challenge page and fill in inputs

--- webDriverV2/Program.cs
+++ webDriverV2/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace webDriverV2
 {
@@ -10,8 +13,25 @@ namespace webDriverV2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://challenge.flinks.io/Authorize/1839410332");
+
+            IWebElement username = driver.FindElement(By.Name("username"));
+            username.SendKeys("1234");
+
+            IWebElement password = driver.FindElement(By.Name("password"));
+            password.SendKeys("1234");
+
+
+            //query.Submit();
+            //var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            //wait.Until(d => d.Title.StartsWith("chesse", StringComparison.OrdinalIgnoreCase));
+            //Console.WriteLine("page tile is: " + driver.Title);
+
+
         }
+        
     }
+
 }
+  
